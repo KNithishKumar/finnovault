727c8ae0cd6b9ef1c8642088c8e3ded5990e95f9
NK <nithish2k4@gmail.com>
2026-08-30 16:12:00 +0530

    Commit 4 (Deleted Package Lock)

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://finnovault-backend.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
