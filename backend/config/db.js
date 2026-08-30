de11c13f64b8bd3916ac6b3e26a2494de62f1cd5
NK <nithish2k4@gmail.com>
2026-08-30 15:32:18 +0530

    Commit 3 (Added MongoDB Atlas)

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/finvault');
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
